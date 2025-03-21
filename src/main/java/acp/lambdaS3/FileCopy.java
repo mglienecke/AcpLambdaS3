037593a561d1872ac3eef9891090857c945d039e
mglienecke <michael@glienecke.de>
2025-03-21 09:22:00 +0000

    File name changes

--- src/main/java/acp/lambdaS3/FileCopy.java
+++ src/main/java/acp/lambdaS3/FileCopy.java
@@ -5,28 +5,12 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.S3Event;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import software.amazon.awssdk.core.ResponseInputStream;
-import software.amazon.awssdk.core.sync.RequestBody;
-import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
-import com.amazonaws.services.lambda.runtime.events.S3Event;
 import com.amazonaws.services.lambda.runtime.events.models.s3.S3EventNotification.S3EventNotificationRecord;
 
-import software.amazon.awssdk.services.s3.S3SystemSetting;
-import software.amazon.awssdk.services.s3.model.GetObjectRequest;
-import software.amazon.awssdk.services.s3.model.GetObjectResponse;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
-import software.amazon.awssdk.services.s3.model.S3Object;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 import java.util.logging.Handler;
-import java.util.stream.Collectors;
 
-public class TextTransformer implements RequestHandler<S3Event, String> {
+public class FileCopy implements RequestHandler<S3Event, String> {
 
     private static final Logger logger = LoggerFactory.getLogger(Handler.class);
 
@@ -42,14 +26,9 @@ public class TextTransformer implements RequestHandler<S3Event, String> {
         String srcBucket = record.getS3().getBucket().getName();
         String srcKey = record.getS3().getObject().getUrlDecodedKey();
 
-        logger.info("Copying " + srcBucket + "/" + srcKey + " to:  " + tgtBucket + "/" + srcKey);
+        logger.info("ACP S3 Lambda Copy :: " + srcBucket + "/" + srcKey + " to:  " + tgtBucket + "/" + srcKey);
 
         try (S3Client s3Client = S3Client.builder().build()) {
-//            for (var setting : S3SystemSetting.values()) {
-//                setting.environmentVariable().equals()
-//            }
-
-
             String finalTgtBucket = tgtBucket;
             s3Client.copyObject(builder -> builder
                     .sourceBucket(srcBucket)
@@ -58,7 +37,7 @@ public class TextTransformer implements RequestHandler<S3Event, String> {
                     .destinationKey(srcKey)
             );
 
-            logger.info("Successfully copied " + srcBucket + "/" + srcKey + " to:  " + tgtBucket + "/" + srcKey);
+            logger.info("ACP S3 Lambda Copy :: SUCCESS -> " + srcBucket + "/" + srcKey + " to:  " + tgtBucket + "/" + srcKey);
         }
 
         return "Done";
